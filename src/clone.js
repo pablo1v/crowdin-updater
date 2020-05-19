86df24c072ea3d97cde28597d866f922fd39f8a0
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-05-19 18:09:08 -0300

    clone path fix

--- src/clone.js
+++ src/clone.js
@@ -7,7 +7,7 @@ async function cloneTranslationRepository() {
   const clonePath = path.resolve(__dirname, '..', cloneUniqueID);
 
   await execSync(
-    `git clone https://github.com/Kaeltec/localization.git${clonePath}`,
+    `git clone https://github.com/Kaeltec/localization.git ${clonePath}`,
     {
       cwd: path.resolve(__dirname, '..'),
     },
